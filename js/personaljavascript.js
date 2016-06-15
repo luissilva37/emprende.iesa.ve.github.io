7a8c72a9aadc82f77c3d073964898cf134e9014e
Luis Eduardo Silva Isturiz <luissilva37@gmail.com>
2016-06-15 14:34:02 -0400

    Sitio Web Emprende IESA 1.0
    Sitio Web Emprende IESA

---
+++ js/personaljavascript.js
@@ -0,0 +1,8 @@
+/* Inicialización de Modal */
+$(document).ready(function(){
+  $('.modal-trigger').leanModal();
+});
+/* Incicialización del Carousel */
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+});
